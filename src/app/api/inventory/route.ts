f9058fb3ccdb4aaf0cd9d3f10a12d4b39f33dacc
shaileshmakwana099 <121852572+shaileshmakwana099@users.noreply.github.com>
2025-05-04 13:58:16 +0530

    Fix error for vercel

--- src/app/api/inventory/route.ts
+++ src/app/api/inventory/route.ts
@@ -1,7 +1,15 @@
 import { NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 export async function POST(request: Request) {
   try {
