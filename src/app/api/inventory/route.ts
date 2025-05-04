c826e5477d25aa00f88ac31458bd045f2f855d4b
shaileshmakwana099 <121852572+shaileshmakwana099@users.noreply.github.com>
2025-05-04 14:02:50 +0530

    Solve errors

--- src/app/api/inventory/route.ts
+++ src/app/api/inventory/route.ts
@@ -1,15 +1,5 @@
 import { NextResponse } from 'next/server';
-import { PrismaClient } from '@prisma/client';
-
-// PrismaClient is attached to the `global` object in development to prevent
-// exhausting your database connection limit.
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
-
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+import { prisma } from '@/lib/prisma';
 
 export async function POST(request: Request) {
   try {
